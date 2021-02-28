b919d26145647dcc1be11ba14e5792f0585405ff
Tachi <andrea@pappacoda.it>
2021-02-28 17:31:36 +0100

    Switch to libcurl Now cloudflare-ddns is faster and much lighter. It now checks and updates a DNS record with less than 20 allocations and using roughly 20 kilobyes of RAM, that is nothing compared to the 300+ allocations of the previous version, that used cpr. cloudflare-ddns now depends only on libcurl and libsimdjson, and should be much easier to package.

--- README.md
+++ README.md
@@ -1,22 +1,22 @@
 # cloudflare-ddns
 
-cloudflare-ddns is a tool that can be used to dynamically update a DNS record using Cloudflare's API. 
+cloudflare-ddns is a tool that can be used to dynamically update a DNS record using Cloudflare's API.
 
 ## Usage
 
-This tool is a oneshot program: you run it, it updates the DNS record, and it terminates. To make it run periodically you could use it with a systemd timer or a cron job.
+This tool is a oneshot program: you run it, it updates the DNS record, and it terminates. To make it run periodically you could use a systemd timer or a cron job.
 
 To run the tool you'll need an [API token](https://dash.cloudflare.com/profile/api-tokens) and the Zone ID of the DNS that you want to update; you can get the latter in the Overview panel of your zone.
 
 ## Dependencies
 
-cloudflare-ddns currently relies on fairly new versions of libcurl and simdjson. To be able to run the utility you need to install `libcurl4` and `libsimdjson5` on your system.
+cloudflare-ddns relies only on libcurl and simdjson. I recommend you to compile the program yourself, but I also provide a statically linked executable for every release. It is not 100% self-contained but it should work in most cases.
 
 ## Build
 
-To build cloudflare-ddns you need to install `meson`, `cmake`, `libcurl4-openssl-dev`, `nlohmann-json3-dev` and a recent version of `libsimdjson-dev`. You should be fine with the packages available in Debian 11 and Ubuntu Hirsute.
+To build cloudflare-ddns you'll need to install `meson`, `pkg-config`, `cmake`, `libcurl4-openssl-dev` and `libsimdjson-dev`. You should be fine with the packages available in Debian 10 (+backports) or Ubuntu Focal.
 
-After having installed the dependencies, you can build the program with `meson setup build --buildtype=relese && meson compile -C build`
+After having installed the dependencies, you can build the program with `meson setup build --buildtype=relese` and then `meson compile -C build`. If your Meson version is too old, you have to run `ninja -C build` instead of `meson compile`.
 
 ## Systemd timer
 
--- cloudflare-ddns/main.cpp
+++ cloudflare-ddns/main.cpp
@@ -1,18 +1,11 @@
-#include <cpr/cpr.h>
 #include <curl/curl.h>
 #include <string>
+#include <iostream>
 #include <simdjson.h>
-#include <nlohmann/json.hpp>
 
-void libcurlSucks() {
-	curl_global_init(CURL_GLOBAL_SSL);
-
-	CURL* curlHandle {curl_easy_init()};
-	curl_easy_setopt(curlHandle, CURLOPT_NOPROGRESS, 1L);
-	curl_easy_setopt(curlHandle, CURLOPT_NOSIGNAL, 1L);
-	curl_easy_setopt(curlHandle, CURLOPT_HTTPGET, 1L);
-
-	curl_global_cleanup();
+std::size_t writeData(char* incomingBuffer, std::size_t size, std::size_t count, std::string* data) {
+	data->append(incomingBuffer, size * count);
+	return size * count;
 }
 
 int main(int argc, char* argv[]) {
@@ -20,40 +13,49 @@ int main(int argc, char* argv[]) {
 		std::cerr << "Usage: cloudflare-ddns <API token> <Zone ID> <DNS record name>\n";
 		return EXIT_FAILURE;
 	}
-	const std::string apiToken {argv[1]};
+	const std::string_view apiToken {argv[1]};
 	const std::string zoneId {argv[2]};
 	const std::string recordName {argv[3]};
+	
+	CURL* curlHandle {curl_easy_init()};
+	std::string response;
+	response.reserve(600);	// Tipical size of largest response (GET to fetch the DNS IP)
+	curl_easy_setopt(curlHandle, CURLOPT_NOPROGRESS, 1L);
+	curl_easy_setopt(curlHandle, CURLOPT_NOSIGNAL, 1L);
+	curl_easy_setopt(curlHandle, CURLOPT_WRITEFUNCTION, writeData);
+	curl_easy_setopt(curlHandle, CURLOPT_WRITEDATA, &response);
+	curl_easy_setopt(curlHandle, CURLOPT_HTTPGET, 1L);
+	curl_easy_setopt(curlHandle, CURLOPT_URL, "https://1.1.1.1/cdn-cgi/trace");
 
+	curl_easy_perform(curlHandle);
+	const std::size_t ipBegin {response.find("ip=") + 3};  // + 3 because "ip=" is 3 chars
+	const std::size_t ipEnd {response.find('\n', ipBegin)};
+	const std::string localIp {response.substr(ipBegin, ipEnd - ipBegin)};
+	response.clear();
+
+	curl_easy_setopt(curlHandle, CURLOPT_URL, std::string{"https://api.cloudflare.com/client/v4/zones/" + zoneId + "/dns_records?type=A&name=" + recordName}.data());
+	curl_easy_setopt(curlHandle, CURLOPT_HTTPAUTH, CURLAUTH_BEARER);
+	curl_easy_setopt(curlHandle, CURLOPT_XOAUTH2_BEARER, apiToken.data());
+	struct curl_slist* headers {nullptr};
+	headers = curl_slist_append(headers, "Content-Type: application/json");
+	curl_easy_setopt(curlHandle, CURLOPT_HTTPHEADER, headers);
+
+	curl_easy_perform(curlHandle);
 	simdjson::dom::parser parser;
-	const simdjson::dom::element parsed {
-		parser.parse(
-			cpr::Get(
-				cpr::Url{"https://api.cloudflare.com/client/v4/zones/" + zoneId + "/dns_records?type=A&name=" + recordName},
-				cpr::Header{
-					{"Content-Type", "application/json"},
-					{"Authorization", "Bearer " + apiToken}
-				}
-				// cpr::Bearer{apiToken} in cpr 1.6
-			).text
-		)
-	};
+	const simdjson::dom::element parsed {parser.parse(response)};
 
-	const std::string trace {cpr::Get(cpr::Url{"https://1.1.1.1/cdn-cgi/trace"}).text};
-	const std::size_t ipBegin {trace.find("ip=") + 3}; // + 3 because "ip=" is 3 chars
-	const std::size_t ipEnd {trace.find('\n', ipBegin)};
-	const std::string_view currentIp {std::string_view{trace}.substr(ipBegin, ipEnd - ipBegin)};  // Begin, length
-
-	if (currentIp != static_cast<std::string_view>((*parsed["result"].begin())["content"])) {
-		std::cout << "New IP: " << (parser.parse(cpr::Patch(
-			cpr::Url{"https://api.cloudflare.com/client/v4/zones/" + zoneId + "/dns_records/" + std::string{static_cast<std::string_view>((*parsed["result"].begin())["id"])}},
-			cpr::Header{
-				{"Content-Type", "application/json"},
-				{"Authorization", "Bearer " + apiToken}
-			},
-			cpr::Body{nlohmann::json{{"content", currentIp}}.dump()}
-		).text))["result"]["content"] << '\n';
+	if (localIp != static_cast<std::string_view>((*parsed["result"].begin())["content"])) {
+		response.clear();
+		curl_easy_setopt(curlHandle, CURLOPT_URL, std::string{"https://api.cloudflare.com/client/v4/zones/" + zoneId + "/dns_records/" + std::string{static_cast<std::string_view>((*parsed["result"].begin())["id"])}}.c_str());
+		// curl_easy_setopt(curlHandle, CURLOPT_NOBODY, 0L);
+		curl_easy_setopt(curlHandle, CURLOPT_CUSTOMREQUEST, "PATCH");
+		std::string request {R"({"content": ")" + localIp + "\"}"};
+		curl_easy_setopt(curlHandle, CURLOPT_POSTFIELDS, request.c_str());
+		curl_easy_perform(curlHandle);
+		std::cout << "New IP: " << parser.parse(response)["result"]["content"] << '\n';
 	}
 	else {
 		std::cout << "The DNS is up to date\n";
 	}
+	curl_easy_cleanup(curlHandle);
 }
