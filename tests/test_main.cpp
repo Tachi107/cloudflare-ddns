8f09bd20926c020f543927f259a6fe9a090f706e
Andrea Pappacoda <andrea@pappacoda.it>
2021-10-25 22:31:52 +0200

    test: add local_ip_test

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -5,7 +5,46 @@
  */
 
 #include <boost/ut.hpp>
+#include <string>
+#include <tachi/cloudflare-ddns.hpp>
+#include <curl/curl.h>
+
+using namespace boost::ut;
+
+extern "C" {
+std::size_t write_data(char* incoming_buffer, const std::size_t size, const std::size_t count, std::string* data) {
+	data->append(incoming_buffer, size * count);
+	return size * count;
+}
+}
+
+CURL* get_test_curl(std::string& response_buffer) {
+	CURL* curl {curl_easy_init()};
+	curl_easy_setopt(
+		curl,
+		CURLOPT_WRITEFUNCTION,
+		write_data
+	);
+	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_buffer);
+	return curl;
+}
 
 int main() {
-	boost::ut::expect(true);
+	curl_global_init(CURL_GLOBAL_DEFAULT);
+
+	"local_ip"_test = [] {
+		std::string response;
+		CURL* curl {get_test_curl(response)};
+
+		curl_easy_setopt(curl, CURLOPT_HTTPGET, 1L);
+		curl_easy_setopt(curl, CURLOPT_URL, "https://icanhazip.com");
+		curl_easy_perform(curl);
+
+		response.pop_back(); // remove \n character
+
+		expect(eq(tachi::get_local_ip(), response));
+		curl_easy_cleanup(curl);
+	};
+
+	curl_global_cleanup();
 }
